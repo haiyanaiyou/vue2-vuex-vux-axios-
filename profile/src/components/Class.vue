404421a8f6daade5a77fd3e751949ccdbc346926
=DingPujiang <=dingpujiang@gujinsuo.com.cn>
2017-08-08 17:50:52 +0800

    事件处理器

---
+++ profile/src/components/Class.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="class">
+    <div class="static" v-bind:class="{ active: isActive, 'text-danger': hasError}">1</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'class',
+  data () {
+    return {
+      isActive: true,
+      hasError: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .static {
+    width: 300px;
+    border: 2px solid bisque;
+  }
+  .active {
+    background: teal;
+  }
+</style>
--- profile/src/components/Hello.vue
+++ profile/src/components/Hello.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li v-for="tab in tabs">
+        {{ tab.text }}
+
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <span v-bind:title="message">
+        悬停几秒后显示title内容
+    </span>
+    <div class="reverse">
+      <p>{{ message }}</p>
+      <button @click="reverse">点击翻转</button>
+
+    </div>
+    <p v-if="seen" @click="hide">
+      now you see
+    </p>
+    <div v-bind:style="styleObject">
+      <p>styleObject</p>
+    </div>
+    <div>
+      <b v-for="n in 10">{{ n }}</b>
+    </div>
+    <div class="click">
+      <button @click="count += 1">点击一次</button>
+      <p>点击的次数 {{ count }}</p>
+    </div>
+    <div style="margin-top:20px;">
+      <button @click="Greet">点击</button>
+    </div>
   </div>
 </template>
 
@@ -25,7 +39,33 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      tabs: [
+        {text: 'aaa'},
+        {text: 'bbb'}
+      ],
+      message: '与' + new Date() + '加载页面',
+      seen: true,
+      styleObject: {
+        color: 'red'
+      },
+      count: 0,
+      name: 'vue js'
+    }
+  },
+  methods: {
+    reverse: function () {
+      this.message = this.message.split(' ').reverse().join(' ')
+    },
+    hide: function () {
+      this.seen = false
+      console.log(this.seen)
+    },
+    Greet: function (event) {
+      alert('hello ' + this.name + '!')
+      if (event) {
+        alert(event.target.tagName)
+      }
     }
   }
 }
@@ -43,11 +83,38 @@ ul {
 }
 
 li {
-  display: inline-block;
+  display: block;
   margin: 0 10px;
 }
 
 a {
   color: #42b983;
+}
+span {
+  display: block;
+  border: 2px dashed darkblue;
+  border-radius: 9px;
+  /* width: 500px; */
+  margin: 10px auto;
+}
+.reverse {
+  margin: 30px auto;
+  /* width: 500px; */
+  border: 2px solid silver;
+  box-shadow: 5px 5px 5px #ccc;
+  border-radius: 9px;
+  padding: 10px;
+
+}
+.click {
+  margin: 20px  auto;
+  border: 2px solid rebeccapurple;
+  border-radius: 6px;
+  padding: 9px;
+}
+button{
+  width: 90px;
+  border: 2px dashed tan;
+  border-radius: 9px;
 }
 </style>
